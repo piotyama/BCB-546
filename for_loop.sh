092a3acf0ecfd41f32edda44ae7bac17a868e10f
Paul Otyama <piotyama@deepak.student.iastate.edu>
2017-02-07 19:12:30 -0600

    README.md

--- for_loop.sh
+++ for_loop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-	for i in {1..10}; 
+for i in {1..10};
+do 
 
-	do
-		awk '$3 ~ /i/ {print}' joined_teosinte.txt | sort -k4,4n > teosinte_chrom$i.txt
-	
-		awk '$3 ~ /i/ {print}' joined_teosinte.txt | sort -k4,4nr > teosinte_chrom$i_r-sort.txt
-	done
+	awk '$3==i {print}' joined_teosinte.txt | sort -k4,4n > teosinte_chrom$i.txt
+
+	awk '$3==i  {print}' joined_teosinte.txt | sort -k4,4nr > teosinte_chrom$i_r-sort.txt
+done
